bef4220018935dd86ed1125d618ac0a9875308e9
Studawatta <st.udawatte@gmail.com>
2023-11-01 05:23:45 +0530

    create user model and auth route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -12,6 +15,22 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use(express.json());
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
