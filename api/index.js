8901715e376f8b1078ae2dedeaef9af0a7f2bd83
Studawatta <st.udawatte@gmail.com>
2023-12-01 23:34:39 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
-
+import path from 'path';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -19,12 +19,20 @@ mongoose
     console.log(err);
   });
 
+const _dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(_dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
